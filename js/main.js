f24a9fed8948c848e69ecaee00f8adb6bf48c83c
Arturo BADILLO ALVARADO <Arturo BADILLO ALVARADO>
2017-03-25 21:43:02 -0500

    agregando pantalla de carga y scroll

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 var main=function () {
     var lastId;
-    var topMenu=$(".nav_menu");
+    var topMenu=$(".menu");
     var topMenuHeigth=topMenu.outerHeight()+1;
-    console.log(topMenuHeigth);
+    console.log("topMenuHeigth"+topMenuHeigth);
     var menuItems=topMenu.find("a");
-
-    console.log(menuItems);
     var scrollItems=menuItems.map(function () {
         var item=$($(this).attr("href"));
         if(item.length){
             return item;
         }
     });
-    console.log(scrollItems);
 
     menuItems.click(function (e) {
         var href=$(this).attr("href");
-        var offsetTop=href==="#"? 0:$(href).offset().top- topMenuHeigth+1;
+        var offsetTop=href==="#"? 0:$(href).offset().top+1;
         console.log(offsetTop);
         $('html, body').stop().animate({
             scrollTop:offsetTop
@@ -25,28 +22,36 @@ var main=function () {
     });
     
     
-    // $(window).scroll(function () {
-    //     var fromTop=$(this).scrollTop()+topMenuHeigth;
-    //     console.log(fromTop);
-    //
-    //     var cur=scrollItems.map(function () {
-    //         if($(this).offset().top<fromTop) return this;
-    //     })
-    //
-    //     // Get the id of the current element
-    //     cur = cur[cur.length-1];
-    //     var id = cur && cur.length ? cur[0].id : "";
-    //
-    //     if (lastId !== id) {
-    //         lastId = id;
-    //         // Set/remove active class
-    //         menuItems
-    //             .parent().removeClass("active")
-    //             .end().filter("[href=#"+id+"]").parent().addClass("active");
-    //     }
-    //
-    // });
+    $(window).scroll(function () {
+         var fromTop=$(this).scrollTop();
+          console.log("From top"+fromTop);
 
+        var cur=scrollItems.map(function () {
+            if($(this).offset().top<(fromTop+10)) return this;
+        })
+        console.log(cur);
+        //
+        // // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+
+         if (lastId !== id) {
+             lastId = id;
+        //     // Set/remove active class
+         console.log(menuItems);
+
+             menuItems
+                 .parent().removeClass("active")
+                 .end().filter("[href=\"#"+id+"\"]").parent().addClass("active");
+         }
+
+    });
+    setTimeout(function(){
+        $('#loader').addClass('loaded');
+        $('.menu').addClass('menu__loaded');
+
+    }, 3000);
 }
 
 $(document).ready(main());
