e4d5ba34326c67ab223be784f9d9fd9c9c9036dc
Arturo BADILLO ALVARADO <Arturo BADILLO ALVARADO>
2017-05-01 16:14:33 -0500

    toggler services

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var main=function () {
     var lastId;
     var topMenu=$(".menu");
     var topMenuHeigth=topMenu.outerHeight()+1;
-    console.log("topMenuHeigth"+topMenuHeigth);
+    //console.log("topMenuHeigth"+topMenuHeigth);
     var menuItems=topMenu.find("a");
     var scrollItems=menuItems.map(function () {
         var item=$($(this).attr("href"));
@@ -14,25 +14,25 @@ var main=function () {
     menuItems.click(function (e) {
         var href=$(this).attr("href");
         var offsetTop=href==="#"? 0:$(href).offset().top+1;
-        console.log(offsetTop);
+        //console.log(offsetTop);
         $('html, body').stop().animate({
             scrollTop:offsetTop
         }, 850);
         e.preventDefault();
     });
-    
+
     $(window).ready(function(){
-      $("html, body").animate({ scrollTop: $("#inicio").scrollTop() }, 1000);  
-    }); 
+      $("html, body").animate({ scrollTop: $("#inicio").scrollTop() }, 1000);
+    });
 
     $(window).scroll(function () {
          var fromTop=$(this).scrollTop();
-          console.log("From top"+fromTop);
+          //console.log("From top"+fromTop);
 
         var cur=scrollItems.map(function () {
             if($(this).offset().top<(fromTop+10)) return this;
         })
-        console.log(cur);
+        //console.log(cur);
         //
         // // Get the id of the current element
         cur = cur[cur.length-1];
@@ -42,7 +42,7 @@ var main=function () {
          if (lastId !== id) {
              lastId = id;
         //     // Set/remove active class
-         console.log(menuItems);
+         //console.log(menuItems);
 
              menuItems
                  .parent().removeClass("active")
@@ -55,6 +55,46 @@ var main=function () {
         $('.menu').addClass('menu__loaded');
 
     }, 3000);
+
+    toggleServices();
+
+    function toggleServices(){
+      var webp=$(".web-page");
+      var corporatei=$(".corporate-identity");
+      var color1="#FFFFFF";
+      var color2="#F8F8F8";
+      webp.hover(function(){
+          corporatei.css({
+            "background-color":color1
+        });
+        webp.css({
+          "background-color":color2
+      });
+      }, function(){
+        corporatei.css({
+          "background-color":color2
+      });
+      webp.css({
+        "background-color":color1
+    });
+      });
+      corporatei.hover(function(){
+          webp.css({
+            "background-color":color2
+        });
+        corporatei.css({
+          "background-color":color1
+      });
+      }, function(){
+        webp.css({
+          "background-color":color1
+      });
+      corporatei.css({
+        "background-color":color2
+    });
+      });
+    }
+
 }
 
 $(document).ready(main());
